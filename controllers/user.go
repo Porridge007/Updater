3de532571fe048faf954810037ded17ce8a7d652
Andrew_Qi <qf353208262@gmail.com>
2020-02-28 10:35:58 +0800

    enc the password when signup

--- controllers/user.go
+++ controllers/user.go
@@ -2,9 +2,14 @@ package controllers
 
 import (
 	"Updater/models"
+	"Updater/util"
 	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
+)
+
+const (
+	pwdSalt = "*#1989"
 )
 
 type UserSignUpController struct {
@@ -23,9 +28,16 @@ func (c *UserSignUpController) Post() {
 	username := c.GetString("username")
 	userpwd := c.GetString("password")
 
+	if len(username) < 3 || len(userpwd) < 5 {
+		c.Ctx.ResponseWriter.Write([]byte("Invalid parameter"))
+		return
+	}
+
+	encPasswd := util.Sha1([]byte(userpwd + pwdSalt))
+
 	user := models.User{
 		UserName: username,
-		UserPwd:  userpwd,
+		UserPwd:  encPasswd,
 	}
 
 	o := orm.NewOrm()
@@ -40,3 +52,14 @@ func (c *UserSignUpController) Post() {
 func (c *UserSignInController) Get() {
 	c.TplName = "signin.tpl"
 }
+
+//func (c *UserSignInController) Post() {
+//	userName := c.GetString("username")
+//	password := c.GetString("password")
+//
+//
+//
+//	encPasswd := util.Sha1([]byte(password + pwdSalt))
+//}
+
+
